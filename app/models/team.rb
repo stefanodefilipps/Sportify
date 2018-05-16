1ab78768720426fbcc8c31ce2fe5b871b021e1c0
Ingegneria Informatica e Automatica <stfn.defilippis@gmail.com>
2018-05-16 18:15:08 +0200

    --- risolti dei bug sui modelli

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ApplicationRecord
 	has_many :membro
 	has_many :user, through: :membro
-	belongs_to :capitano, class_name "User"
+	belongs_to :capitano, class_name: "User"
 	has_many :pt
 	has_many :tt
 end
